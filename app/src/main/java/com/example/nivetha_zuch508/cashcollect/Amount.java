ef00884f182f7da6c1fd6fe8f72e7ef8e69eb046
vignesh <vigneshkm00@gmail.com>
2018-05-18 01:04:49 +0530

    api call

--- app/src/main/java/com/example/nivetha_zuch508/cashcollect/Amount.java
+++ app/src/main/java/com/example/nivetha_zuch508/cashcollect/Amount.java
@@ -20,6 +20,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class Amount extends AppCompatActivity {
     EditText amn;
@@ -68,7 +69,18 @@ public class Amount extends AppCompatActivity {
                     databaseref.child(key).child(year).child(y+m+month).setValue(artist);
                     Toast.makeText(getApplicationContext(), "Paid", Toast.LENGTH_LONG).show();
                     Intent j = new Intent(Amount.this, bill_list.class);
-                   // sendSms();
+                    String y;
+                    Calendar c = Calendar.getInstance();
+                    int a = c.get(Calendar.YEAR);
+                    int b = c.get(Calendar.DATE);
+                    int d = c.get(Calendar.HOUR);
+                    int e = c.get(Calendar.MINUTE);
+                    int f = c.get(Calendar.SECOND);
+                    String g = c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+                    String message = "&message=" + "Recharge of Rs."+amn1+" for "+month+" is successful on "+b+g+","+a+" "+d+":"+e+":"+f+" towards your cable connection with customer ID:"+key+".";
+                    Toast.makeText(getApplicationContext(), "Paid:"+message, Toast.LENGTH_LONG).show();
+                    y = sendSms();
+                    Toast.makeText(getApplicationContext(), "Sms Sent:"+y, Toast.LENGTH_LONG).show();
                     setResult(1, j);
                     finish();
                 }
@@ -84,6 +96,41 @@ public class Amount extends AppCompatActivity {
     }
     public String sendSms() {
         try {
+            // Construct data
+            Calendar c = Calendar.getInstance();
+            int a = c.get(Calendar.YEAR);
+            int b = c.get(Calendar.DATE);
+            int d = c.get(Calendar.HOUR);
+            int e = c.get(Calendar.MINUTE);
+            int f = c.get(Calendar.SECOND);
+            String g = c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+            String apiKey = "apikey=" + "coM+EvHnSM8-NmPxMWt9BHMKS8OxatxNDr2WFFbczE";
+            String message = "&message=" + "Recharge of Rs."+amn1+" for "+month+" is successful on "+b+g+","+a+" "+d+":"+e+":"+f+" towards your cable connection with customer ID:"+key+".";
+            String sender = "&sender=" + "TXTLCL";
+            String numbers = "&numbers=" + "91"+key;
+
+            // Send data
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
+            String data = apiKey + numbers + message;
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            conn.getOutputStream().write(data.getBytes("UTF-8"));
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+            rd.close();
+
+            return stringBuffer.toString();
+        } catch (Exception e) {
+            System.out.println("Error SMS "+e);
+            Toast.makeText(getApplicationContext(),"Error SMS:"+e,Toast.LENGTH_LONG).show();
+            return "Error "+e;
+        }
+      /*  try {
             // Construct data
             String apiKey = "apikey=" + "M75sYdRhU+o-veY4GL4RXoXTyrjyhwao6OQ6Ut6O26";
             String message = "&message=" + "The Bill Amount for Customer ID:"+key+"is Rs."+amount+".paid:"+amn1+" Status:"+status+".";
@@ -109,7 +156,6 @@ public class Amount extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println("Error SMS "+e);
             return "Error "+e;
-        }
+        }*/
     }
-
 }
